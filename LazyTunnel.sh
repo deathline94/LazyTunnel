b755d5683c4d8f27992d3a8f1982bd43b14ac6a5
deathline94 <dlb4all@gmail.com>
2023-05-24 08:58:11 +0330

    v2.1

--- LazyTunnel.sh
+++ LazyTunnel.sh
@@ -3,21 +3,16 @@
 SERVICE_FILE="/etc/systemd/system/iptables.service"
 IP_FILE="/root/ip.txt"
 
-# Function to copy script to /root directory
-copy_script() {
-  SCRIPT_PATH=$(readlink -f "$0")
-  SCRIPT_NAME=$(basename "$0")
-  TARGET_PATH="/root/${SCRIPT_NAME}"
-
-  if [ "${SCRIPT_PATH}" != "${TARGET_PATH}" ]; then
-    install -m 755 "${SCRIPT_PATH}" "${TARGET_PATH}"
-  fi
+# Function to create a temporary copy of the script
+create_temp_copy() {
+  TEMP_COPY=$(mktemp)
+  cp "$0" "$TEMP_COPY"
+  chmod +x "$TEMP_COPY"
+  echo "$TEMP_COPY"
 }
 
 # Function to install IPTables rules and set up service
 install() {
-  copy_script
-
   mainland_ip=$(curl -s https://api.ipify.org)
   echo "Mainland IP Address (automatically detected): ${mainland_ip}"
   read -p "Foreign IP Address: " foreign_ip
@@ -27,9 +22,9 @@ install() {
   echo "${foreign_ip}" >> "${IP_FILE}"
 
   # Set up IPTables rules
-  sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination "${mainland_ip}"
-  sudo iptables -t nat -A PREROUTING -j DNAT --to-destination "${foreign_ip}"
-  sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+  iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination "${mainland_ip}"
+  iptables -t nat -A PREROUTING -j DNAT --to-destination "${foreign_ip}"
+  iptables -t nat -A POSTROUTING -j MASQUERADE
 
   # Create and enable systemd service
   echo "[Unit]
@@ -56,35 +51,4 @@ uninstall() {
   foreign_ip=$(tail -n 1 "${IP_FILE}")
 
   # Remove IPTables rules
-  sudo iptables -t nat -D PREROUTING -p tcp --dport 22 -j DNAT --to-destination "${mainland_ip}"
-  sudo iptables -t nat -D PREROUTING -j DNAT --to-destination "${foreign_ip}"
-  sudo iptables -t nat -D POSTROUTING -j MASQUERADE
-
-  # Clear IPTables rules and policies
-  sudo iptables -F
-  sudo iptables -X
-  sudo iptables -t nat -F
-  sudo iptables -t nat -X
-  sudo iptables -t mangle -F
-  sudo iptables -t mangle -X
-  sudo iptables -P INPUT ACCEPT
-  sudo iptables -P FORWARD ACCEPT
-  sudo iptables -P OUTPUT ACCEPT
-
-  # Stop and disable the service
-  sudo systemctl stop iptables
-  sudo systemctl disable iptables
-
-  # Remove service file and IP file
-  sudo rm -f "${SERVICE_FILE}"
-  sudo rm -f "${IP_FILE}"
-
-  echo "Uninstallation complete."
-}
-
-# Main script logic
-if [[ "$1" == "uninstall" ]]; then
-  uninstall
-else
-  install
-fi
+  iptables -t nat -D PRER
